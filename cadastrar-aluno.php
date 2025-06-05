547cb2c28135ede2ed9357d39a4291abaa93909a
navarrosemeraro <semerarogiovanni05@gmail.com>
2025-06-05 11:24:19 -0300

    Cabeçalho Completo (Pastas corretamente estruturadas)

--- cadastrar-aluno.php
+++ cadastrar-aluno.php
@@ -3,5 +3,14 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db_name = "correcao_redacao";
+$charset = 'utf8mb4';
+$variavel ='Global';
+teste();
+echo ('Valor: ' .$variavel. '!<br>');
+
+function teste(){
+    global $variavel;
+    $variavel ='Local';
+}
 
 ?>
