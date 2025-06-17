f6cf873f286e2d1655d4950e1612d600ddf675d7
navarrosemeraro <semerarogiovanni05@gmail.com>
2025-06-17 11:58:26 -0300

    Todas as telas preenchidas (ícone do cp2 colocado)

--- consulta.php
+++ consulta.php
@@ -6,7 +6,7 @@ $db_name = "automacao";
 $charset = 'utf8mb4';
 $variavel ='Global';
 
-$sql = "SELECT";
+$sql = "SELECT DISTINCT 'alunos.nome', 'alunos.matricula', 'redacao.id', 'redacao.tema', 'redacao.texto_arquivo', FROM 'alunos' JOIN 'redacao';";
 
 $conn = new mysqli($servername, $username, $password, $db_name, $charset);
 
@@ -20,12 +20,10 @@ else{
     }
     else{
         while($row == $result->fetch_assoc){
-            echo ( "<tr>
-            <th>" . $row["nome"]
-
-        );
+            echo ('<option value="' . $row["redacao.id"] . '">' . $row["alunos.nome"] . $row["alunos.matricula"] . $row["redacao.tema"] . $row["redacao.texto_arquivo"] . '</option>');
         }
     }
+    echo ("</select>");
 }
 $conn->close();
 
