340a223af62feb2e769ca4ce4e46b2d68bfa74d5
navarrosemeraro <semerarogiovanni05@gmail.com>
2025-08-24 22:36:53 -0300

    visualização da redação do aluno CONCLUIDA e FUNCIONAL (falta melhorar os comentários)

--- pages/pages_aluno/minhas_redacoes/visualizar_redacao_selecionada.php
+++ pages/pages_aluno/minhas_redacoes/visualizar_redacao_selecionada.php
@@ -89,63 +89,108 @@ include '../../../php/global/db.php';
                             <div style="display: flex; gap: 200px; align-items:last baseline; justify-content: center;">
                                 <div class="container text-center">
                                     <div class="row">
-                                        <div class="col" style=" padding: 0;">
-                                            <label><b>Competência 1:
-                                                    <?php echo $row["nota_comp1"]; ?>
-                                                </b></label><br>
+                                       <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="bolinha1" class="bolas_comp">C1</div>
+                                                <label>Nota:</label><br>
+                                                <label><?php echo $row["nota_comp1"]; ?></label>
+                                            </div>
+                                            <div style="float:right; width:100%;">
+                                                <label style="font-size:16px; text-align:left;"><b>Demonstrar domínio da norma culta da língua portuguesa</b></label><br><br>
+                                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" data-nota="<?= $row['nota_comp1']; ?>">
+                                                    <div class="progress-bar" style="background-color: #3B82F6;"></div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <br><br>
                                     <div class="row">
-                                        <div class="col" style="padding: 0;">
-                                            <label><b>Competência 2:
-                                                    <?php echo $row["nota_comp2"]; ?>
-                                                </b></label><br>
+                                        <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="bolinha2" class="bolas_comp">C2</div>
+                                                <label>Nota:</label><br>
+                                                <label><?php echo $row["nota_comp2"]; ?></label>
+                                            </div>
+                                            <div style="float:right; width:100%;">
+                                                <label style="font-size:16px; text-align:left;"><b>Compreender proposta e aplicar diferentes conhecimentos</b></label><br><br>
+                                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" data-nota="<?= $row['nota_comp2']; ?>">
+                                                    <div class="progress-bar" style="background-color: #10B981;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br><br>
+                                    <div class="row">
+                                        <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="bolinha3" class="bolas_comp">C3</div>
+                                                <label>Nota:</label><br>
+                                                <label><?php echo $row["nota_comp3"]; ?></label>
+                                            </div>
+                                            <div style="float:right; width:100%;">
+                                                <label style="font-size:16px; text-align:left;"><b>Organização e clareza da argumentação</b></label><br><br>
+                                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" data-nota="<?= $row['nota_comp3']; ?>">
+                                                    <div class="progress-bar" style="background-color: #8B5CF6;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br><br>
+                                    <div class="row">
+                                        <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="bolinha4" class="bolas_comp">C4</div>
+                                                <label>Nota:</label><br>
+                                                <label><?php echo $row["nota_comp4"]; ?></label>
+                                            </div>
+                                            <div style="float:right; width:100%;">
+                                                <label style="font-size:16px; text-align:left;"><b>Demonstrar conhecimento dos mecanismos linguísticos</b></label><br><br>
+                                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" data-nota="<?= $row['nota_comp4']; ?>">
+                                                    <div class="progress-bar" style="background-color: #F97316;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br><br>
+                                    <div class="row">
+                                        <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="bolinha5" class="bolas_comp">C5</div>
+                                                <label>Nota:</label><br>
+                                                <label><?php echo $row["nota_comp5"]; ?></label>
+                                            </div>
+                                            <div style="float:right; width:100%;">
+                                                <label style="font-size:16px; text-align:left;"><b>Proposta de solução para o problema abordado</b></label><br><br>
+                                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" data-nota="<?= $row['nota_comp5']; ?>">
+                                                    <div class="progress-bar" style="background-color: #EC4899;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br><br>
+                                    <div class="row">
+                                        <div class="col d-flex flex-column flex-md-row gap-4" style="padding: 0; align-items:start;">
+                                            <div style="float:left;">
+                                                <div id="div_nota">
+                                                    <label>Nota Total: </label><br>
+                                                    <label style="margin-left: 5px; padding:4px; border: 1px white solid"><?php echo $row["nota_total"]; ?></label>
+                                                </div>
 
+                                            </div>
                                         </div>
-                                    </div>
-                                    <br><br>
-                                    <div class="row">
-                                        <div class="col" style="padding: 0;">
-                                            <label><b>Competência 3
-                                                    <?php echo $row["nota_comp3"]; ?>:
-                                                </b></label><br>
-
-                                        </div>
-                                    </div>
-                                    <br><br>
-                                    <div class="row">
-                                        <div class="col" style="padding: 0;">
-                                            <label><b>Competência 4:
-                                                    <?php echo $row["nota_comp4"]; ?>
-                                                </b></label><br>
-                                        </div>
-                                    </div>
-                                    <br><br>
-                                    <div class="row">
-                                        <div class="col" style="padding: 0;">
-                                            <label><b>Competência 5:
-                                                    <?php echo $row["nota_comp5"]; ?>
-                                                </b></label><br>
-                                        </div>
-                                    </div>
-                                    <br><br>
-                                    <div class="row">
-                                        <div style="display: flex; align-items: center; gap: 10px;">
-                                            <label for="nota_tot" class="form-label"
-                                                style="margin: 0;"><b>Total: <?php echo $row["nota_total"];?> </b></label>
-                                        </div><br>
                                     </div>
                                 </div>
                             </div>
 
         </section>
+        <br>
+        <section id="dois">
         <div class="container-xxl d-flex flex-column" id="div-correcao2">
             <div class="w-100 w-md-auto" style="flex: 1;">
                 <fieldset class="border border-2 p-4 w-100">
                     <div style="display: flex; gap: 200px; align-items:last baseline; justify-content: center;">
                         <div class="row" style="text-align: left;">
-                            <label for="comentario_corretor"><b>Comentários do Corretor(a):</b></label>
+                            <label for="comentario_corretor" style="margin"><b>Comentários do Corretor(a):</b></label><br>
                             <textarea class="form-control" name="comentario_corretor" id="comentario_corretor" rows="6" cols="100"
                                 readonly><?php echo $row["observacoes"] ?? "Nenhum Comentário..."; ?></textarea>
                         </div><br>
@@ -154,6 +199,7 @@ include '../../../php/global/db.php';
             </fieldset>
         </div>
         </div>
+        </section>
 
     </main>
 
@@ -168,7 +214,23 @@ include '../../../php/global/db.php';
         document.getElementById("tema_redacao_txt").innerText = tema_redacao;
 
     </script>
+    <script>
+        // Define a nota máxima que uma competência pode ter
+        const max = 200;
+
+        // Seleciona todas as divs com a classe "progress" (containers das barras)
+        document.querySelectorAll('.progress').forEach(container => {
+    
+        // Lê a nota que foi inserida no atributo "data-nota" pelo PHP
+        let nota = container.getAttribute('data-nota');
+    
+        // Converte a nota em porcentagem em relação ao máximo (ex.: 150/200 = 75%)
+        let percent = (nota / max) * 100;
+    
+        // Acessa a div interna ".progress-bar" e aplica a largura proporcional
+        container.querySelector('.progress-bar').style.width = percent + "%";
+});
+    </script>
 </body>
 
 </html>
-?>
