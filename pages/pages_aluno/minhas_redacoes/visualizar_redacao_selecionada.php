ad2cb51393e3ce8bd13312a35198f4c3e569f212
navarrosemeraro <semerarogiovanni05@gmail.com>
2025-08-30 08:45:19 -0300

    requirements.txt (permite que acesse as bibliotecas utilizadas em outra máquina)

--- pages/pages_aluno/minhas_redacoes/visualizar_redacao_selecionada.php
+++ pages/pages_aluno/minhas_redacoes/visualizar_redacao_selecionada.php
@@ -10,7 +10,7 @@ include '../../../php/global/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../assets/common/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../assets/common/visualiza_redacao/visualiza_redacao.php">
+    <link rel="stylesheet" href="../../../assets/common/visualiza_redacao/visualiza_redacao.css">
     <link rel="icon" type="image/png" href="../../../assets/aluno/img/global/Brasão_Colégio_Pedro_II.png" />
     <title>Cadastro de Corretores</title>
 </head>
